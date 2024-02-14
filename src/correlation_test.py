ad5a222194b46b9f03dce6f902b849d1f5365ac0
Kunal Rout <2kunal6@gmail.com>
2024-02-14 18:52:46 +0530

    correlation

---
+++ src/correlation_test.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+# using numpy ---------------------------------------------------------
+x = np.array([1,3,5,7,8,9, 10, 15])
+y = np.array([10, 20, 30, 40, 50, 60, 70, 80])
+
+print(np.corrcoef(x, y))
+
+
+# using pandas
+
+import pandas as pd
+from sklearn.datasets import load_diabetes
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = load_diabetes(as_frame=True)
+df = df.frame
+df.head()
+corr = df.corr()
+print(corr)
